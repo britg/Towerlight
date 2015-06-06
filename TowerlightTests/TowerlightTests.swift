9ccf9aa110f7c5c08366db66c1a1d81af8d41ea6
britg <brit@britg.com>
2015-06-06 07:43:15 -0500

    Initial Commit

---
+++ TowerlightTests/TowerlightTests.swift
@@ -0,0 +1,36 @@
+//
+//  TowerlightTests.swift
+//  TowerlightTests
+//
+//  Created by Brit Gardner on 6/6/15.
+//  Copyright (c) 2015 Brit Gardner. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class TowerlightTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
